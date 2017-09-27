6cdbeb397e990b6be27389839dc3c1cde5a3e317
Julie <padilla410@gmail.com>
2017-09-27 11:22:47 -0400

    Removing .R that aren't ready

--- R/globalVariables.R
+++
@@ -1,1 +0,0 @@
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
--- R/y_labeller.R
+++
@@ -1,4 +0,0 @@
-y_axis_labeller <- function(param = NULL) {
-  # add some code to format a y axis label based on the param
-
-}
