45223987a7d44ba78777ed54e0d8bc7d27f37dce
Julie <padilla410@gmail.com>
2017-10-05 20:21:51 -0400

    historical_daily_range
    First submit of historical_daily_range.R

--- R/historical_daily_range.R
+++ R/historical_daily_range.R
@@ -1,6 +1,6 @@
-#' Historical Monthly/Seasonal Range Timeseries
+#' Historical Daily Range Timeseries
 #'
-#' Monthly average and variability. Looking at variability within each month; no historical context
+#' Daily averages and variability. Looking at variability within a year, with historical context
 #'
 #' @param swmpr_in input swmpe object
 #' @param param chr string of variable to plot
@@ -9,14 +9,15 @@
 #' @param criteria numeric, a numeric criteria that will be plotted as a horizontal line
 #' @param log_trans logical, should y-axis be log? Defaults to \code{FALSE}
 #' @param plot logical, should a plot be returned? Defaults to \code{TRUE}
-#' @param ... additional arguments passed to other methods. See \code{\link{assign_season}}
+#' @param ... additional arguments passed to other methods (not used for this function).
 #'
 #' @concept analyze
 #'
-#' @import ggplot2 dplyr scales rlang
+#' @import ggplot2 dplyr rlang
 #'
 #' @importFrom magrittr "%>%"
-#' @importFrom lubridate  year floor_date
+#' @importFrom lubridate  year floor_date yday
+#' @importFrom scales comma
 #'
 #' @export
 #'
@@ -39,26 +40,26 @@
 #'
 #' dat <- qaqc(apacpwq, qaqc_keep = c('0', '3', '5'))
 #' # with criteria
-#' y <- historical_range(dat, param = 'do_mgl', target_yr = 2013, criteria = 2, abb = T)
+#' y <- historical_daily_range(dat, param = 'do_mgl', target_yr = 2013, criteria = 2, abb = T)
 #'
 #' # w/o criteria
-#' x <- historical_range(dat, param = 'do_mgl', target_yr = 2013, abb = T)
+#' x <- historical_daily_range(dat, param = 'do_mgl', target_yr = 2013, abb = T)
 #'
 #' # add a y label
 #' x <- x + labs(x = NULL, y = "Dissolved Oxygen (mg/L)")
 #' }
 
-historical_range <- function(swmpr_in, ...) UseMethod('historical_range')
+historical_daily_range <- function(swmpr_in, ...) UseMethod('historical_daily_range')
 
-#' @rdname historical_range
+#' @rdname historical_daily_range
 #'
 #' @concept analyze
 #'
 #' @export
 #'
-#' @method historical_range swmpr
+#' @method historical_daily_range swmpr
 #'
-historical_range.swmpr <- function(swmpr_in
+historical_daily_range.swmpr <- function(swmpr_in
                                    , param = NULL
                                    , hist_rng = NULL
                                    , target_yr = NULL
@@ -75,6 +76,7 @@ historical_range.swmpr <- function(swmpr_in
   avg <- sym('mean')
   mini <- sym('min')
   maxi <- sym('max')
+  jd <- sym('julian_day')
 
   rng <- hist_rng
 
@@ -83,14 +85,16 @@ historical_range.swmpr <- function(swmpr_in
   station <- attr(dat, 'station')
 
   #TESTS
-  #determine type WQ, MET, NUT
-  if(substr(station, 6, nchar(station)) == 'nut')
-    warning('Nutrient data detected. Consider specifying seasons > 1 month. See `?assign_season` for details.')
-
-  #determine historical range exists, if not default to min/max of the range
+  #determine historical range exists and that it is reasonable, if not default to min/max of the range
   if(is.null(rng)) {
     warning('No historical range specified. Entire time series will be used.')
     rng <- c(min(lubridate::year(dat$datetimestamp)), max(lubridate::year(dat$datetimestamp)))
+  } else {
+    if(min(rng) < min(lubridate::year(dat$datetimestamp)) | max(rng) > max(lubridate::year(dat$datetimestamp))) {
+      warning('Specified range is greater than the range of the dataset. Max/min  range of the dataset will be used.')
+      rng <- c(min(lubridate::year(dat$datetimestamp)), max(lubridate::year(dat$datetimestamp)))
+    }
+
   }
 
   #determine that variable name exists
@@ -110,7 +114,7 @@ historical_range.swmpr <- function(swmpr_in
 
   # Filter to historic range
   dat <- dat %>% dplyr::filter(lubridate::year(.data$datetimestamp) >= rng[[1]]
-                                    & lubridate::year(.data$datetimestamp) <= rng[[2]])
+                               & lubridate::year(.data$datetimestamp) <= rng[[2]])
 
   # Assign date for determining daily stat value
   dat$date <- lubridate::floor_date(dat$datetimestamp, unit = 'days')
@@ -126,47 +130,48 @@ historical_range.swmpr <- function(swmpr_in
                      , min = min(!! parm, na.rm = TRUE)
                      , max = max(!! parm, na.rm = TRUE))
 
-  # Assign seasons
-  dat_all$season <- assign_season(dat_all$date, ...)
+  dat_all$julian_day <- lubridate::yday(dat_all$date)
 
   # Determine average min/max/mean for each month (for all years together)
-  dat_hist <- dat_all %>%
-    dplyr::group_by(!! seas) %>%
+  dat_hist_avg <- dat_all %>%
+    dplyr::group_by(!! jd) %>%
     dplyr::summarise(mean = mean(!! avg, na.rm = T)
                      , min = mean(!!  mini, na.rm = T)
                      , max = mean(!! maxi, na.rm = T))
+
+  dat_hist_obs <- dat_all %>%
+    dplyr::group_by(!! jd) %>%
+    dplyr::summarise(mean = mean(!! avg, na.rm = T)
+                     , min = min(!!  mini, na.rm = T)
+                     , max = max(!! maxi, na.rm = T))
 
   dat_yr <- dat_all %>% dplyr::filter(year(date) == target_yr)
 
-  dat_yr <- dat_yr %>%
-    dplyr::group_by(!! seas) %>%
-    dplyr::summarise(mean = mean(!! avg, na.rm = T)
-                     , min = mean(!!  mini, na.rm = T)
-                     , max = mean(!! maxi, na.rm = T))
+  if(length(dat_yr[1, ] < 365)) {
+    jday_fill <- data.frame(julian_day = c(1:365))
+    dat_yr <- suppressMessages(dplyr::left_join(jday_fill, dat_yr))
+  }
 
   if(plot){
     # Set the plot range
-    mx <- max(dat_hist$max, na.rm = T)
+    mx <- max(dat_hist_obs$max, na.rm = T)
     mx <- ceiling(mx)
     mn <- ifelse(log_trans == TRUE, 0.1, 0)
+    brks <- c(1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335) #jdays associated with the first of every month
+    brk_labs <- month.abb
 
     # Make some labels
-    lab_hist_rng <- paste(rng[[1]], '-', rng[[2]], ' Daily Avg Range', sep = '')
-    lab_hist_ln <- paste(rng[[1]], '-', rng[[2]], ' Daily Avg', sep = '')
-    lab_yr_rng <- paste(target_yr, ' Daily Avg Range', sep = '')
+    lab_hist_avg_rng <- paste(rng[[1]], '-', rng[[2]], ' Daily Avg Range', sep = '')
+    lab_hist_obs_rng <- paste(rng[[1]], '-', rng[[2]], ' Daily Range', sep = '')
     lab_yr_ln <- paste(target_yr, ' Daily Avg', sep = '')
 
     # Make plot
     plt <-
-      ggplot(data = dat_yr, aes_(x = seas, y = avg, group = 1)) +
-      geom_ribbon(data = dat_hist, aes_(x = seas, ymin = mini, ymax = maxi
-                                        , fill = lab_hist_rng, alpha = lab_hist_rng)) +
-      geom_ribbon(data = dat_yr, aes_(x = seas, ymin = mini, ymax = maxi
-                                      , fill = lab_yr_rng, alpha = lab_yr_rng)) +
-      geom_line(data = dat_hist, aes_(x = seas, y = avg, color = lab_hist_ln)
-                , linetype = 'solid', lwd = 1) +
-      geom_line(color = 'steelblue3', lwd = 1) +
-      geom_point(aes_(fill = lab_yr_ln, shape = lab_yr_ln), size = 2) +
+      ggplot(data = dat_yr, aes_(x = jd, y = avg, group = 1)) +
+      geom_ribbon(data = dat_hist_obs, aes_(x = jd, ymin = mini, ymax = maxi, fill = lab_hist_obs_rng)) +#, alpha = lab_hist_avg_rng)) +
+      geom_ribbon(data = dat_hist_avg, aes_(x = jd, ymin = mini, ymax = maxi, fill = lab_hist_avg_rng)) + #, alpha = lab_hist_obs_rng)) +
+      geom_line(aes(color = lab_yr_ln), lwd = 1.5) +
+      scale_x_continuous(breaks = brks, labels = brk_labs) +
       labs(x = NULL, y = NULL) +
       theme_bw() +
       theme(legend.position = 'top', legend.direction = 'horizontal')
@@ -174,27 +179,25 @@ historical_range.swmpr <- function(swmpr_in
     # Adjust scale
     plt <-
       plt +
-      scale_color_manual('', values = c('gray40')) +
-      scale_fill_manual('', values = c('gray40', 'steelblue3', 'steelblue3'), guide = F) +
-      scale_shape_manual('', values = c(21)) +
-      scale_alpha_manual('', values = rep(0.25, 2))
+      scale_color_manual('', values = c('steelblue3')) +
+      scale_fill_manual('', values = c('gray80', 'gray60'))
 
     # Override legend defaults
+    # plt <-
+    #   plt +
+    #   guides(alpha = guide_legend(override.aes = list(fill = c('steelblue3', 'gray40')), order = 3, reverse = T)
+    #          , shape = guide_legend(override.aes = list(fill = 'steelblue3'), order = 1)
+    #          , color = guide_legend(override.aes = list(color = 'gray40'), order = 2))
+
+    # Adjust theme
     plt <-
       plt +
-      guides(alpha = guide_legend(override.aes = list(fill = c('steelblue3', 'gray40')), order = 3, reverse = T)
-             , shape = guide_legend(override.aes = list(fill = 'steelblue3'), order = 1)
-             , color = guide_legend(override.aes = list(color = 'gray40'), order = 2))
-
-      # Adjust theme
-      plt <-
-        plt +
-        theme(panel.grid.major = element_blank(),
+      theme(panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(),
             strip.background = element_blank(),
             panel.border = element_rect(color = 'black')) +
-        theme(axis.title.y = element_text(margin = unit(c(0, 8, 0, 0), 'pt'), angle = 90)) +
-        theme(text = element_text(size = 16))
+      theme(axis.title.y = element_text(margin = unit(c(0, 8, 0, 0), 'pt'), angle = 90)) +
+      theme(text = element_text(size = 16))
 
     # Adjust legend keys and spacing
     plt <-
@@ -208,18 +211,18 @@ historical_range.swmpr <- function(swmpr_in
 
       plt <- plt +
         geom_hline(aes(yintercept = criteria, linetype = factor('WQ Threshold'))
-                       , color = 'red', show.legend = T) +
+                   , color = 'red', show.legend = T) +
         scale_linetype_manual('', values = c('longdash'))
 
       plt <-
         plt +
-        guides(alpha = guide_legend(override.aes = list(fill = c('steelblue3', 'gray40'), linetype = 0), order = 3, reverse = T)
-               , shape = guide_legend(override.aes = list(fill = 'steelblue3', linetype = 0), order = 1)
-               , color = guide_legend(override.aes = list(color = 'gray40'), order = 2)
-               , linetype = guide_legend(override.aes = list(color = 'red'), order = 4))
+        guides(fill = guide_legend(override.aes = list(linetype = 0), order = 2, reverse = T)
+               , color = guide_legend(override.aes = list(color = 'steelblue3'), order = 1)
+               , linetype = guide_legend(override.aes = list(color = 'red'), order = 3))
     }
 
     return(plt)
+    # return(dat_yr)
 
   } else {
 
