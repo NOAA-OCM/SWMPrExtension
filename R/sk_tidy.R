5a542ecc70a2ca0e275e6a64390d4a7464388190
Julie <padilla410@gmail.com>
2017-11-14 19:23:02 -0500

    sk_tidy

---
+++ R/sk_tidy.R
@@ -0,0 +1,24 @@
+#' Tidy Seasonal Kendall Results
+#'
+#' Tidy results from \code{EnvStats::kendallSeasonalTrendTest}
+#'
+#' @param data a vector of POSIXct dates
+#'
+#'
+#' @examples
+#' \dontrun{
+#' data(elksmwq)
+#'
+#' }
+#'
+#' @export
+#'
+#' @details A helper function used by \code{sk_monthly} to return a table of tidied values.
+#'
+#' @return a \code{data.frame} of results from \code{kendallSeasonalTrendTest}
+#'
+#'
+#'
+sk_tidy <- function() {
+
+}
