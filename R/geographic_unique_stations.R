22fd890359d86863f1d183df78eea6c5187abcbf
Julie <padilla410@gmail.com>
2018-03-09 20:40:25 -0500

    updating return value

--- R/geographic_unique_stations.R
+++ R/geographic_unique_stations.R
@@ -27,19 +27,13 @@ geographic_unique_stations <- function(nerr_site_id) {
 
   loc <- get('sampling_stations')
 
-  # check for valid nerr_site_ids
-  if(!(nerr_site_id %in% loc$Station.Code))
-    stop("Station does not appear to be a valid NERR sampling station. Inspect data(sampling_stations) for details.")
-
   # generate location labels
   loc <- loc[(loc$Station.Code %in% nerr_site_id), ]
   loc$abbrev <- substr(loc$Station.Code, start = 1, stop = 5)
 
-  loc <- unique(loc[, c('abbrev', 'Longitude', 'Latitude')])
+  loc <- loc[!duplicated(loc[ , c('abbrev', 'Longitude', 'Latitude')]),]#unique(loc[, c('abbrev', 'Longitude', 'Latitude'), ])
 
-  loc <- loc[order(loc$abbrev), ]
-
-  loc <- loc[[1]]
+  loc <- sort(loc$Station.Code)
 
   return(loc)
 }
