10c4707496a50e813a63e3254f66a04bd5543a97
Dave <OldFORTRANdog@users.noreply.github.com>
2020-05-09 21:30:30 -0400

    Update res_sk_map.R

--- R/res_sk_map.R
+++ R/res_sk_map.R
@@ -10,9 +10,10 @@
 #' @param station_labs logical, should stations be labeled? Defaults to \code{TRUE}
 #' @param lab_loc chr vector of 'R' and 'L', one letter for each station. if no \code{lab_loc} is specified then labels will default to the left.
 #' @param scale_pos scale_pos where should the scale be placed? Options are 'topleft', 'topright', 'bottomleft', or 'bottomright'. Defaults to 'bottomleft'
+#' @param zoom zoom level, 1-21 for stamen maps. Default is to autoscale based on bbox.
+#' @param maptype stamen map type from ggmap::get_stamenmap.  One of c("terrain", "terrain-background", "terrain-labels", "terrain-lines", "toner", "toner-2010", "toner-2011", "toner-background", "toner-hybrid", "toner-labels", "toner-lines", "toner-lite", "watercolor")
 #'
-#' @import leaflet
-#'
+#' @importFrom ggmap get_stamenmap ggmap
 #' @importFrom ggthemes theme_map
 #' @importFrom magrittr "%>%"
 #' @importFrom maptools elide spRbind unionSpatialPolygons
@@ -80,76 +81,76 @@ res_sk_map <- function(nerr_site_id
                        , shp
                        , station_labs = TRUE
                        , lab_loc = NULL
-                       , scale_pos = 'bottomleft') {
+                       , scale_pos = 'bottomleft'
+                       , zoom = NULL
+                       , maptype = 'toner-lite') {
 
   #------------------Uncomment for debugging------------------------------------
-  library(SWMPrExtension)
-  library(sf)
-  library(dplyr)
-  library(tmap)
-  library(tmaptools)
-  library(ggmap)
-  # library(osmplotr)
-  FIRST <- TRUE
-  if(FIRST){
-    ### DEBUG variables
-    # Defaults
-    station_labs = TRUE
-    # from Example 1, a compact reserve
-    stations <-
-    sampling_stations[(sampling_stations$NERR.Site.ID == 'elk'
-                       & sampling_stations$Status == 'Active'), ]$Station.Code
-    to_match <- c('wq')
-    stns <- stations[grep(paste(to_match, collapse = '|'), stations)]
-    shp_fl <- elk_spatial
-    bounding_elk <- c(-121.810978, 36.868218, -121.708667, 36.764050)
-    pos <- 'bottomleft'
-    sk_res <- c('inc', 'dec', 'dec', 'insig')
-
-    ### plot call, reassign variables
-    ### res_sk_map('elk', stations = stns, sk_result = sk_result,
-    ###            bbox = bounding_elk, scale_pos = pos, shp = shp_fl)
-    nerr_sit_id <- 'elk'
-    stations <- stns
-    sk_result <- sk_res
-    bbox <- bounding_elk
-    lab_loc <- NULL
-    scale_pos <- pos
-    shp <- shp_fl
-    } else {
-    # ---------------------------------------------------------------------------
-    # Second Example
-    # Defaults
-    station_labs <- TRUE
-    scale_pos <- 'bottom_left'
-    ## a multicomponent reserve (showing two different bounding boxes)
-    ### set plotting parameters
-    stations <-
-      sampling_stations[(sampling_stations$NERR.Site.ID == 'cbm'
-                         & sampling_stations$Status == 'Active'), ]$Station.Code
-    to_match <- c('wq')
-    stns <- stations[grep(paste(to_match, collapse = '|'), stations)]
-    shp_fl <- cbm_spatial
-    bounding_cbm_1 <- c(-77.393, 39.741, -75.553, 38.277)
-    bounding_cbm_2 <- c(-76.862006, 38.811571, -76.596508, 38.642454)
-    pos <- 'bottomleft'
-    sk_res <- c('inc', 'dec', 'dec', 'insig')
-
-    ### plot
-    # res_sk_map('cbm', stations = stns, sk_result = sk_result, bbox = bounding_cbm_1,
-    #            scale_pos = pos, shp = shp_fl)
-    #
-    # res_sk_map('cbm', stations = stns, sk_result = sk_result, bbox = bounding_cbm_2,
-    #            scale_pos = pos, shp = shp_fl)
-    nerr_sit_id <- 'cbm'
-    stations <- stns
-    bbox <- bounding_cbm_2
-    lab_loc <- NULL
-    scale_pos <- pos
-    shp <- shp_fl
-
-    }
-  #---------------end debugging-----------------------------------------------
+  # library(SWMPrExtension)
+  # library(sf)
+  # library(dplyr)
+  # library(ggmap)
+  # # library(osmplotr)
+  # FIRST <- FALSE
+  # if(FIRST){
+  #   ### DEBUG variables
+  #   # Defaults
+  #   station_labs = TRUE
+  #   # from Example 1, a compact reserve
+  #   stations <-
+  #   sampling_stations[(sampling_stations$NERR.Site.ID == 'elk'
+  #                      & sampling_stations$Status == 'Active'), ]$Station.Code
+  #   to_match <- c('wq')
+  #   stns <- stations[grep(paste(to_match, collapse = '|'), stations)]
+  #   shp_fl <- elk_spatial
+  #   bounding_elk <- c(-121.810978, 36.868218, -121.708667, 36.764050)
+  #   pos <- 'bottomleft'
+  #   sk_res <- c('inc', 'dec', 'dec', 'insig')
+  #
+  #   ### plot call, reassign variables
+  #   ### res_sk_map('elk', stations = stns, sk_result = sk_result,
+  #   ###            bbox = bounding_elk, scale_pos = pos, shp = shp_fl)
+  #   nerr_sit_id <- 'elk'
+  #   stations <- stns
+  #   sk_result <- sk_res
+  #   bbox <- bounding_elk
+  #   lab_loc <- NULL
+  #   scale_pos <- pos
+  #   shp <- shp_fl
+  #   } else {
+  #   # ---------------------------------------------------------------------------
+  #   # Second Example
+  #   # Defaults
+  #   station_labs <- TRUE
+  #   scale_pos <- 'bottom_left'
+  #   ## a multicomponent reserve (showing two different bounding boxes)
+  #   ### set plotting parameters
+  #   stations <-
+  #     sampling_stations[(sampling_stations$NERR.Site.ID == 'cbm'
+  #                        & sampling_stations$Status == 'Active'), ]$Station.Code
+  #   to_match <- c('wq')
+  #   stns <- stations[grep(paste(to_match, collapse = '|'), stations)]
+  #   shp_fl <- cbm_spatial
+  #   bounding_cbm_1 <- c(-77.393, 39.741, -75.553, 38.277)
+  #   bounding_cbm_2 <- c(-76.862006, 38.811571, -76.596508, 38.642454)
+  #   pos <- 'bottomleft'
+  #   sk_res <- c('inc', 'dec', 'dec', 'insig')
+  #
+  #   ### plot
+  #   # res_sk_map('cbm', stations = stns, sk_result = sk_result, bbox = bounding_cbm_1,
+  #   #            scale_pos = pos, shp = shp_fl)
+  #   #
+  #   # res_sk_map('cbm', stations = stns, sk_result = sk_result, bbox = bounding_cbm_2,
+  #   #            scale_pos = pos, shp = shp_fl)
+  #   nerr_sit_id <- 'cbm'
+  #   stations <- stns
+  #   bbox <- bounding_cbm_2
+  #   lab_loc <- NULL
+  #   scale_pos <- pos
+  #   shp <- shp_fl
+  #
+  #   }
+  # #---------------end debugging-----------------------------------------------
   # check that a shape file exists
     if(class(shp) != 'SpatialPolygons') {
     if(class(shp) != 'sf') {
@@ -186,14 +187,6 @@ res_sk_map <- function(nerr_site_id
   if(!is.null(lab_loc))
     loc$align[lab_loc == 'R'] <- 1.25
 
-  # # set map label styles
-  # label_style <- list(
-  #   "box-shadow" = "none",
-  #   "border-radius" = "5px",
-  #   "font" = "bold 16px/1.5 'Helvetica Neue', Arial, Helvetica, sans-serif",
-  #   "padding" = "1px 5px 1px 5px"
-  #   )
-
   # order selected stations alphabetically
   loc <- loc[order(loc$Station.Code), ]
 
@@ -206,33 +199,31 @@ res_sk_map <- function(nerr_site_id
   # Now transform into the projected web-mercator projection EPSG:3857
   #loc_sf <- sf::st_transform(loc_sf, 3857)
 
-  # Determine the types of results
-  if('inc' %in% loc$sk_result){inc_icons <- grep('inc', loc$sk_result)}
-  if('dec' %in% loc$sk_result){dec_icons <- grep('dec', loc$sk_result)}
-  if('insig' %in% loc$sk_result){insig_icons <- grep('insig', loc$sk_result)}
-  if('insuff' %in% loc$sk_result){insuff_icons <- grep('insuff', loc$sk_result)}
-
   # Define vectors for the colors, shapes and sizes as needed:
-  #   first and second entries are for states, "regular" and "highlighted,"
-  #   respectively; third and forth entries are for reserve locations,
-  #   "regular" and "highlighted" respecitvely;
-  #   5 - 8 are for showing S-K trend results: 5 = increasing, 6 = decreasing,
-  #   7 = insignificant, and 8 = insufficient data.
+  #   1 - 4 are for showing S-K trend results: 1 = increasing, 2 = decreasing,
+  #   3 = insignificant, and 4 = insufficient data.
   # This convention holds for colors, shapes and size parameters. The order is
   #   consistent with the original order.
 
   fill_colors <-  c('#444E65', '#A3DFFF', '#247BA0', '#0a0a0a')
-#  res_point_size <-   c(4.9,  4.9,  4.5, 4.2)
-  res_point_size <-   c(5,5,5,5)
+  res_point_size <-   c(8,8,8,8)
   res_point_shape <-  c(24, 25, 21, 13)
 
   # These are the codes for the fill color, size and shape legends.
   break_vals <- c("inc", "dec", "insig", "insuff")
 
+  # Set background map zoom level automatically if not specified
+  if(is.null(zoom)) {
+    diag_size <- sqrt((xmax-xmin)^2 +(ymax-ymin)^2)
+    zoom <- 13 - ceiling(sqrt(10*diag_size))
+    print(paste("Zoom level calculated as", zoom, sep = " "))
+  }
+  print(paste("maptype is ",maptype))
+
   bg_map <- ggmap::get_stamenmap(bbox,
-                                 maptype = "toner-lite",
+                                 maptype = maptype,
                                  source = "stamen",
-                                 zoom = 13,
+                                 zoom = zoom,
                                  urlonly = FALSE)
 
   m <- ggmap(bg_map) +
@@ -250,7 +241,6 @@ res_sk_map <- function(nerr_site_id
     scale_fill_manual(values = fill_colors, breaks = break_vals) +
     scale_size_manual(values = res_point_size, breaks = break_vals) +
     scale_shape_manual(values = res_point_shape, breaks = break_vals)
-
 
   return(m)
 }
